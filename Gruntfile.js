72e26ecd018226a02973f2d55fd0186564fd921d
katapo <kata.pochec@gmail.com>
2017-09-19 00:18:51 +0200

    .gitignore added

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'css/style.css': 'style.sass'
+          'style.css': 'style.sass'
         }
       }
     },
